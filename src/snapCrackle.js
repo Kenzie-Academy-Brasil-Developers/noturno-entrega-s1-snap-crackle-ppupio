0a06f5b579d0877bbbd624be0feab478b1ea144f
ppupio <tarsopupio@gmail.com>
2021-10-25 20:04:06 -0300

    Modiicacao sem primos pronta

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,33 @@
 // Seu código vai aqui
+let part
+let phrase=[]
+let text=""
+function snapCrackle (maxValue) {
+   
+    for (let counter=1; counter<=maxValue; counter++){
+        if (counter%5===0 && counter%2!==0) {
+        part = "snapCrackle"
+        text=text+part+", "
+        phrase.push(part)
+        }
+        else {if (counter%5===0 && counter%2===0) {
+        part = "Crackle"
+        text=text+part+", "
+        phrase.push(part)
+        }
+        else {if (counter%2!==0) {
+            part = "snap"
+            text=text+part+", "
+            phrase.push(part)
+        }
+        else {
+            part = counter
+            text=text+part+", "
+            phrase.push(part)
+        }
+    }
+    }
+  }
+    return text
+}
+console.log (snapCrackle(35))
